b507ca220c79d773ab589a310351a7c1b0bd356d
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-01-16 00:52:29 -0500

    first piece of homework

---
+++ Algorithms/Karatsuba.py
@@ -0,0 +1,28 @@
+import math
+"""
+implement Karatsuba multiplication
+
+"""
+
+
+def multiply(x, y):
+    if len(x) == 1 or len(y) == 1:
+        return int(x)*int(y)
+    middle_of_x = int(len(x)/2)
+    a = x[:middle_of_x]
+    b = x[middle_of_x:]
+
+    middle_of_y = int(len(y)/2)
+    c = y[:middle_of_y]
+    d = y[middle_of_y:]
+
+    step_one = multiply(c, a)
+    step_two = multiply(d, b)
+    step_three = multiply(str(int(a)+int(b)), str(int(c)+int(d)))
+    gauss_trick = step_three - step_two - step_one
+    return math.pow(10, len(x)) * step_one + math.pow(10, middle_of_x) * gauss_trick + step_two
+
+first_num = '3141592653589793238462643383279502884197169399375105820974944592'
+second_num = '3141592653589793238462643383279502884197169399375105820974944592'
+print(multiply(first_num, second_num))
+print(int(first_num) * int(second_num))
