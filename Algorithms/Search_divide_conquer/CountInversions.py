f655c097ef1504be00de2432481d8fa2b9ab22ed
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-01-16 18:55:13 -0500

    unresolved inversions issue

---
+++ Algorithms/Search_divide_conquer/CountInversions.py
@@ -0,0 +1,50 @@
+"""
+Script to count inversions
+using mergeSort routine in this directory as the skeleton
+"""
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    half = int(len(arr)/2)
+    first_sorted_half, x = merge_sort(arr[:half])
+    second_sorted_half, y = merge_sort(arr[half:])
+    merged_halves, z = merge(first_sorted_half, second_sorted_half)
+
+    return merged_halves, x + y + z
+
+
+def merge(first_half, second_half):
+    n = len(first_half) + len(second_half)
+    i = 0
+    j = 0
+    split_inversions = 0
+    ans = []
+    for k in range(n):
+        if i >= len(first_half):
+            ans.append(second_half[j])
+            j += 1
+            continue
+        if j >= len(second_half):
+            ans.append(first_half[i])
+            split_inversions += len(first_half) - i
+            i += 1
+            continue
+
+        if first_half[i] > second_half[j]:
+            ans.append(second_half[j])
+            j += 1
+            split_inversions += len(first_half) - i
+        elif first_half[i] < second_half[j]:
+            ans.append(first_half[i])
+            i += 1
+
+    return ans, split_inversions
+
+
+input_file = open('Inversions.txt', 'r')
+numbers = [int(x) for x in input_file.read().split()]
+inversions = merge_sort(numbers)
+print(inversions[1])
+
