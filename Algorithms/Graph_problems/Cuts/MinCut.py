fd67c65e8d8086087fa8c21c5306f4ce5abbf69d
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-01-19 23:52:52 -0500

    started Karger

---
+++ Algorithms/Graph_problems/Cuts/MinCut.py
@@ -0,0 +1,14 @@
+"""
+Implementation of Karger's MinCut Algorithm
+the input is an adjacency list of the format
+x (node ) followed by n nodes adjacent to it
+Example:
+    1 2 3 4
+    2 4 1
+    3 4 1
+    4 3 2 1
+represents a square with vertices numbered clockwise
+the square has a diagonal edge (1,4)
+"""
+
+
--- Algorithms/Search_divide_conquer/Quick_Sort/RandomizedSelection.py
+++ Algorithms/Search_divide_conquer/Quick_Sort/RandomizedSelection.py
@@ -1,5 +1,42 @@
+import random
 """
 Canonical implementation of Randomized selection
 to find the nth order statistic in a list of numbers
+piggy-backing on quicksort
 """
 
+
+def quick_sort(arr, n=0):
+    quick_sort_helper(arr, 0, len(arr)-1, n)
+
+
+def quick_sort_helper(arr, start, end, n):
+    if end - start < 1:
+        return
+    p = partition(arr, start, end)
+    if p == n + 1:
+        print(arr[n])
+        return
+    elif p > n + 1:
+        quick_sort_helper(arr, start, p - 1, n)
+    else:
+        quick_sort_helper(arr, p + 1, end, n)
+
+
+def partition(arr, start, end):
+    # partition around a random pivot, return index of the pivot
+    pivot_index = random.randint(start, end)
+    pivot = arr[pivot_index]
+    i = start + 1
+    arr[start], arr[pivot_index] = arr[pivot_index], arr[start]
+    for j in range(start + 1, end + 1):
+        if arr[j] < pivot:
+            arr[j], arr[i] = arr[i], arr[j]
+            i += 1
+
+    arr[i - 1], arr[start] = arr[start], arr[i - 1]
+    return i - 1
+
+nums = [6, 5, 4, 3, 2, 1]
+quick_sort(nums, 3) #should print 2
+print(nums)
