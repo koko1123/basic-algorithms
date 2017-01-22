e7173ff46cb897201560e47d59af616553357ed2
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-01-21 22:10:07 -0500

    Karger read routine

--- Algorithms/Graph_problems/Cuts/MinCut.py
+++ Algorithms/Graph_problems/Cuts/MinCut.py
@@ -1,15 +1,7 @@
 import random
+import copy
 """
 Implementation of Karger's MinCut Algorithm
-the input is an adjacency list of the format
-x (node ) followed by n nodes adjacent to it
-Example:
-    1 2 3 4
-    2 4 1
-    3 4 1
-    4 3 2 1
-represents a square with vertices numbered clockwise
-the square has a diagonal edge (1,4)
 """
 
 
@@ -18,6 +10,7 @@ def karger_algorithm(adjacency_list):
         random_u = random.choice(adjacency_list.keys())
         random_v = random.choice(adjacency_list[random_u])
         contraction(random_u, random_v, adjacency_list)
+
     return len(adjacency_list[list(adjacency_list.keys())[0]])
 
 
@@ -26,8 +19,22 @@ def contraction(u, v, adjacency_list):
         if adjacent_node != v:
             adjacency_list[v].append(adjacent_node)
             adjacency_list[adjacent_node].append(v)
+
         adjacency_list[adjacency_node].remove(u)
+
     adjacency_list.remove(u)
 
 
+def read_graph():
+    file = open('kargerMinCut.txt', 'r')
+    adjacency_graph = {}
+    for line in file:
+        node = int(line.split()[0])
+        adjacent_nodes = [int(x) for x in line.split()[1:]]
+        adjacency_graph[node] = adjacent_nodes
+    return adjacency_graph
+
+
+graph = read_graph()
+print(graph)
 
