96ebc582d534085ce08221c625e8cd99e243c217
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-01-29 13:06:52 -0500

    efficient connected components

--- Algorithms/Graph_problems/BreadthFirstSearch.py
+++ Algorithms/Graph_problems/BreadthFirstSearch.py
@@ -10,7 +10,7 @@ def breadth_first_search(graph):
     count = 0
     for i in range(len(graph)):
         if i not in visited:
-            #start BFS
+            # start BFS
             bfs_queue.put(i)
             visited.add(i)
             count += 1
@@ -18,9 +18,9 @@ def breadth_first_search(graph):
                 row_num = bfs_queue.get()
                 visited.add(row_num)
                 for j in range(len(graph)):
-                    if graph[row_num][j] is 1 and row_num!=j and j not in visited:
+                    if graph[row_num][j] is 1 and row_num != j and j not in visited:
                         bfs_queue.put(j)
-    print(count)
+    return count
 
 connected_graph = [[1, 0, 0, 0, 1, 1],
                    [0, 1, 1, 0, 0, 0],
@@ -28,4 +28,4 @@ connected_graph = [[1, 0, 0, 0, 1, 1],
                    [0, 0, 1, 1, 0, 0],
                    [1, 0, 0, 0, 1, 1],
                    [0, 0, 0, 0, 1, 1]]
-breadth_first_search(connected_graph)
+print(breadth_first_search(connected_graph) == 2)
