9ee8cd459e4eeaf3916567f82ff027aef21826a1
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-07-08 16:43:00 -0400

    additional files added, old karatsuba multiplication corrected

--- Algorithms/search-divide-conquer/Karatsuba.py
+++ Algorithms/search-divide-conquer/Karatsuba.py
@@ -7,7 +7,7 @@ implement Karatsuba multiplication
 
 
 def multiply(x, y):
-    if len(x) == 1 or len(y) == 1:
+    if len(x) in [1, 2] or len(y) in [1, 2]:
         return int(x) * int(y)
     middle_of_x = int(len(x) / 2)
     a = x[:middle_of_x]
@@ -21,7 +21,9 @@ def multiply(x, y):
     step_two = multiply(d, b)
     step_three = multiply(str(int(a) + int(b)), str(int(c) + int(d)))
     gauss_trick = step_three - step_two - step_one
-    return math.pow(10, len(x)) * step_one + math.pow(10, middle_of_x) * gauss_trick + step_two
+    return int(math.pow(10, len(x)) * step_one + math.pow(10, middle_of_x) * gauss_trick + step_two)
 
-# first_num = '3141592653589793238462643383279502884197169399375105820974944592'
-# second_num = '3141592653589793238462643383279502884197169399375105820974944592'
+
+first_num = '3141592653589793238462643383279502884197169399375105820974944592'
+second_num = '3141592653589793238462643383279502884197169399375105820974944592'
+print(multiply(first_num, second_num))
