dadd971f9b4a644b650ae77d80c6530914ce5395
Amlandeep Bhadra <amlandeep1912@gmail.com>
2017-07-09 23:20:34 -0400

    additional files added, old karatsuba multiplication corrected

--- Algorithms/greedy-algorithms/JobSchedulingAlgorithm.py
+++ Algorithms/greedy-algorithms/JobSchedulingAlgorithm.py
@@ -5,14 +5,25 @@ prioritization includes a (weight - length) implementation and a (weight/length)
 
 
 def create_difference_schedule(jobs):
-    return sorted(jobs, key=lambda x: (x[0] - x[1], -x[0]))
+    sorted(jobs, key=lambda x: (x[0] - x[1], -x[0]))
+    return jobs
 
 
 def create_ratio_schedule(jobs):
-    # tie breaker doesnt matter
-    return jobs.sort(key=lambda x: int(x[0] / x[1]))
+    sorted(jobs, key=lambda x: int((-x[0] / x[1]) * 100))
+    return jobs
 
 
 def compute_weighted_completion_time(schedule):
-    # Todo: complete completion time calc routine
-    print(schedule)
+    completion_time = 0
+    completion_time_scores = []
+    for job in schedule:
+        completion_time += job[1]
+        completion_time *= job[0]
+        completion_time_scores.append(completion_time)
+    return sum(completion_time_scores)
+
+
+job_list = [(1, 2), (3, 5)]
+print(compute_weighted_completion_time(create_difference_schedule(job_list)))
+print(compute_weighted_completion_time(create_ratio_schedule(job_list)))
